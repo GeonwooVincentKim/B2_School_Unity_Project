dab20c940212f209871fffc3d3e91a9bacbe2cf7
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-08 21:11:02 +0900

    Add Delta-Time into this Project

---
+++ Assets/LifeCycle_2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Translate means Multiplies to Vector.
+        // Vector Function : Multiplies Vector Function to Time Parameter.
+        Vector3 vec = new Vector3(
+            Input.GetAxisRaw("Horizontal") * Time.deltaTime,
+            Input.GetAxisRaw("Vertical") * Time.deltaTime
+        );
+        transform.Translate(vec);
+    }
+}
